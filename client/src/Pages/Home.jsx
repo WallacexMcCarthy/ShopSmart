63f1ec04a7d615c46bbc082b92e3fd6b2986ba9a
Wallace McCarthy <wallacemccarthy@protonmail.com>
2024-10-27 07:41:39 -0600

    deployment change

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -94,7 +94,7 @@ const HomePage = () => {
     try {
       console.log('Fetching from /ebay');
 
-      const res = await fetch('/ebay', {
+      const res = await fetch('https://shopsmart-e0gg.onrender.com/ebay', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ product, pages }),
