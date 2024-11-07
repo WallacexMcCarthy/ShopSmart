d9c42354dfbd39bd3aadc0852a2da967018dd1a3
Wallace McCarthy <wallacemccarthy@protonmail.com>
2024-11-07 13:12:37 -0700

    Revert "Update main.py"
    This reverts commit 55195d789fecbfa9f7db38ebd061501f8d487a74.

--- backend/main.py
+++ backend/main.py
@@ -7,11 +7,8 @@ from macys_scraper import MacysScraper
 
 app = Flask(__name__)
 
-# Enable CORS for the specified origin (your Netlify site)
 CORS(app, resources={r"/ebay": {"origins": "https://shopssmart.netlify.app"}})
-CORS(app, resources={r"/amazon": {"origins": "https://shopssmart.netlify.app"}})
-CORS(app, resources={r"/target": {"origins": "https://shopssmart.netlify.app"}})
-CORS(app, resources={r"/macys": {"origins": "https://shopssmart.netlify.app"}})
+
 
 @app.route('/')
 def index():
@@ -29,7 +26,7 @@ def get_ebay_products():
         pages = request.json['pages']
         result = EbayScraper(str(product), int(pages)).scrape()
         
-        scraping = {str(product): result}  
+        scraping = {str(product): result }  
         
         print(scraping)
         return jsonify(scraping) 
@@ -37,16 +34,16 @@ def get_ebay_products():
     else:
         result = EbayScraper("microwave", 1).scrape()
         scraping = {"products": result}
-        return jsonify(scraping)
-
-@app.route("/amazon", methods=['GET', 'POST'])
+        return jsonify(scraping) 
+    
+@app.route("/amazon", methods = ['GET', 'POST'])
 def get_amazon_products():
     if request.method == 'POST':
         product = request.json['product']
         pages = request.json['pages']
         result = AmazonScraper(str(product), int(pages)).scrape()
         
-        scraping = {str(product): result}  
+        scraping = {str(product): result }  
         
         print(scraping)
         return jsonify(scraping) 
@@ -54,16 +51,16 @@ def get_amazon_products():
     else:
         result = AmazonScraper("microwave", 1).scrape()
         scraping = {"products": result}
-        return jsonify(scraping)
-
-@app.route("/target", methods=['GET', 'POST'])
+        return jsonify(scraping) 
+    
+@app.route("/target", methods = ['GET', 'POST'])
 def get_target_products():
     if request.method == 'POST':
         product = request.json['product']
         pages = request.json['pages']
         result = TargetScraper(str(product), int(pages)).scrape()
         
-        scraping = {str(product): result}  
+        scraping = {str(product): result }  
         
         print(scraping)
         return jsonify(scraping) 
@@ -71,16 +68,16 @@ def get_target_products():
     else:
         result = TargetScraper("microwave", 1).scrape()
         scraping = {"products": result}
-        return jsonify(scraping)
-
-@app.route("/macys", methods=['GET', 'POST'])
+        return jsonify(scraping) 
+    
+@app.route("/macys", methods = ['GET', 'POST'])
 def get_macys_products():
     if request.method == 'POST':
         product = request.json['product']
         pages = request.json['pages']
         result = MacysScraper(str(product), int(pages)).scrape()
         
-        scraping = {str(product): result}  
+        scraping = {str(product): result }  
         
         print(scraping)
         return jsonify(scraping) 
@@ -88,8 +85,11 @@ def get_macys_products():
     else:
         result = MacysScraper("microwave", 1).scrape()
         scraping = {"products": result}
-        return jsonify(scraping)
+        return jsonify(scraping) 
+    
+
+
+
 
 if __name__ == "__main__":
-    # Ensure your Flask app is accessible over the network
     app.run(host="0.0.0.0", port=5000, debug=True)
