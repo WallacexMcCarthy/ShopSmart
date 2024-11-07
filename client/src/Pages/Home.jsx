9ecabeccf0b2c9bc9da5636be20320459518f559
Wallace McCarthy <wallacemccarthy@protonmail.com>
2024-11-07 13:12:53 -0700

    Revert "deployment change"
    This reverts commit 63f1ec04a7d615c46bbc082b92e3fd6b2986ba9a.

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -94,7 +94,7 @@ const HomePage = () => {
     try {
       console.log('Fetching from /ebay');
 
-      const res = await fetch('https://shopsmart-e0gg.onrender.com/ebay', {
+      const res = await fetch('/ebay', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ product, pages }),
